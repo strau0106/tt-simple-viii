3f3f67bcffa520bb25e018bf178cff283c62e3ac
Rufus Spyra <rufus.spyra@mygymer.ch>
2024-08-06 14:06:35 +0200

    feat(memory): Add program counter to memory.

--- src/modules/memory.sv
+++ src/modules/memory.sv
@@ -3,32 +3,47 @@ import control::*;
 
 module memory (
     input bit clock,
+    input bit reset,
     input bit[7:0] in,
     output bit[7:0] out,
     input bit data_word_selector,
+    input bit bus_selector, // 0: MAR, 1: PC
     input memory_op_e op
 );
 
-    bit[(`ADDR_BUS_WIDTH-1):0] addr_reg;
+    bit[(`ADDR_BUS_WIDTH-1):0] memory_address_register;
+    bit[(`ADDR_BUS_WIDTH-1):0] programm_counter;
+    bit[(`ADDR_BUS_WIDTH-1):0] selected_bus;
+
+    assign selected_bus = bus_selector ? programm_counter : memory_address_register;
+
     bit[7:0] cells [(1<<`ADDR_BUS_WIDTH+1)-1];
 
     bit[7:0] out_tmp;
+
     assign out = (op == READ) ? out_tmp : 'z;
 
     always_ff @(posedge clock) begin
+        if (reset) begin
+            memory_address_register <= 0;
+            programm_counter <= 0;
+        end else
         case (op)
             default:
                 ; // no operation
             READ:
-                out_tmp <= cells[{addr_reg, data_word_selector}];
+                out_tmp <= cells[{selected_bus, data_word_selector}];
             WRITE:
-                cells[{addr_reg, data_word_selector}] <= in;
+                cells[{selected_bus, data_word_selector}] <= in;
             ABSOLUTE:
-                addr_reg <= {1'b0, in};
+                if (bus_selector) programm_counter <= {1'b0, in};
+                else memory_address_register <= {1'b0, in};
             REL_SUB:
-                addr_reg <= addr_reg - {1'b0, in};
+                if (bus_selector) programm_counter <= programm_counter - {1'b0, in};
+                else memory_address_register <= memory_address_register - {1'b0, in};
             REL_ADD:
-                addr_reg <= addr_reg + {1'b0, in};
+                if (bus_selector) programm_counter <= programm_counter + {1'b0, in};
+                else memory_address_register <= memory_address_register + {1'b0, in};
         endcase
     end
 endmodule

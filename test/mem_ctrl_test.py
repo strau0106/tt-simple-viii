ca5a5a33cbe9030921bcb561115a6bbde4b79baa
Rufus Spyra <rufus.spyra@mygymer.ch>
2025-02-22 17:32:55 +0100

    test: Remove unnnecessary over/underflow test for addr regs.

--- test/mem_ctrl_test.py
+++ test/mem_ctrl_test.py
@@ -82,27 +82,3 @@ async def test_write_increment(dut):
         await RisingEdge(dut.clock)
         assert dut.addr_out.value == 0x21+((1<<24) if addr == ADDR_SEL.MAR else 0), "Increment address write failed"
 
-@cocotb.test()
-async def test_overflow_underflow(dut):
-    clock = Clock(dut.clock, 10, units="ns")
-    cocotb.start_soon(clock.start())
-    
-    for addr in ADDR_SEL:
-        await reset(dut)
-        dut.addr_reg_op.value = ADDR_REG_OP.ABSOLUTE.value
-        dut.addr_sel.value = addr.value
-        dut.bus_data_in.value = 0xFFFFFFF0
-        await RisingEdge(dut.clock)
-        await RisingEdge(dut.clock)
-        
-        dut.addr_reg_op.value = ADDR_REG_OP.REL_ADD.value
-        dut.bus_data_in.value = 0x20
-        await RisingEdge(dut.clock)
-        await RisingEdge(dut.clock)
-        assert dut.addr_out.value == (0xFFFFFFF0 + 0x20) & 0xFFFFFFFF, "Overflow address write failed"
-        
-        dut.addr_reg_op.value = ADDR_REG_OP.REL_SUB.value
-        dut.bus_data_in.value = 0x30
-        await RisingEdge(dut.clock)
-        await RisingEdge(dut.clock)
-        assert dut.addr_out.value == (0xFFFFFFF0 + 0x20 - 0x30) & 0xFFFFFFFF, "Underflow address write failed"

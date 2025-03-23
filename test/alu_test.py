d6578fe374c3b7439e4ab5ef5f0cb81b42a9596c
Rufus Spyra <rufus.spyra@mygymer.ch>
2025-03-23 20:20:43 +0100

    test: Add scan_chain based alu_tests.

--- test/alu_test.py
+++ test/alu_test.py
@@ -6,9 +6,7 @@ from collections import namedtuple
 
 Param = namedtuple('Param', ["op", "reg1", "reg2", "expected_result", "expected_carry", "expected_zero", "test_name"])
 
-@cocotb.test()
-async def test_turing_req_2111_arithmetics(dut):
-    params = [
+arithmetic_params = [
         Param(ALU_OP.ADD, 0, 0, 0, 0, 1, "Zero"),  # Zero
         Param(ALU_OP.ADD, 3, 2, 5, 0, 0, "Basic functionality"),  # Basic functionality
         Param(ALU_OP.ADD, 255, 1, 0, 1, 1, "Roll over, carry flag, zero flag"),  # Roll over, carry flag, zero flag
@@ -19,7 +17,10 @@ async def test_turing_req_2111_arithmetics(dut):
         Param(ALU_OP.SUB, 0, 1, 255, 1, 0, "Borrow, carry flag"),  # Borrow, carry flag
         Param(ALU_OP.SUB, 1, 1, 0, 0, 1, "Result zero, zero flag")  # Result zero, zero flag
     ]
-    for param in params:
+
+@cocotb.test()
+async def test_turing_req_2111_arithmetics(dut):
+    for param in arithmetic_params:
         dut.register1.value = param.reg1
         dut.register2.value = param.reg2
         dut.op.value = param.op.value  # Assuming ADD is encoded as 0
@@ -29,9 +30,7 @@ async def test_turing_req_2111_arithmetics(dut):
         assert dut.is_zero.value == param.expected_zero, f"Failed: {param.test_name}"
         assert dut.is_carry.value == param.expected_carry, f"Failed: {param.test_name}"
 
-@cocotb.test()
-async def test_feature_req_2114_logic(dut):
-    params = [
+logic_params = [
         # AND operation
         Param(ALU_OP.AND, 0b01010111, 0b00110010, 0b00010010, 0, 0, "AND Basic functionality"),  # Basic functionality
         Param(ALU_OP.AND, 0b11111111, 0b00000000, 0b00000000, 0, 1, "AND Zero result"),  # Zero result
@@ -53,7 +52,9 @@ async def test_feature_req_2114_logic(dut):
         Param(ALU_OP.NOT, 0b11111111, 0, 0b00000000, 0, 1, "NOT All bits set, zero result"),  # All bits set, zero result
     ]
 
-    for param in params:
+@cocotb.test()
+async def test_feature_req_2114_logic(dut):
+    for param in logic_params:
         dut.register1.value = param.reg1
         dut.register2.value = param.reg2
         dut.op.value = param.op.value
@@ -63,9 +64,8 @@ async def test_feature_req_2114_logic(dut):
         assert dut.is_zero.value == param.expected_zero, f"Failed: {param.test_name}"
         assert dut.is_carry.value == param.expected_carry, f"Failed: {param.test_name}"
 
-@cocotb.test()
-async def test_feature_req_2115_shift(dut):
-    params = [
+
+shift_params = [
         # ROR operation
         Param(ALU_OP.ROR, 0b00000001, 0, 0b10000000, 0, 0, "ROR Rotate right"),  # Rotate right
         Param(ALU_OP.ROR, 0b10000001, 0, 0b11000000, 0, 0, "ROR Rotate right with carry"),  # Rotate right with carry
@@ -87,7 +87,10 @@ async def test_feature_req_2115_shift(dut):
         Param(ALU_OP.SHL, 0b00000000, 0, 0b00000000, 0, 1, "SHL Zero input"),  # Zero input
     ]
 
-    for param in params:
+@cocotb.test()
+async def test_feature_req_2115_shift(dut):
+
+    for param in shift_params:
         dut.register1.value = param.reg1
         dut.register2.value = param.reg2
         dut.op.value = param.op.value
--- test/top_ops_test.py
+++ test/top_ops_test.py
@@ -4,6 +4,7 @@ from cocotb.triggers import RisingEdge
 from lib.utils import reset
 from lib.scan_chain import ScanPattern, shift_pattern_in, shift_pattern_out
 from lib.controlpack import CTRL_STATE, ALU_OP, MUX_SEL, REGISTERS_OP
+from alu_test import arithmetic_params, logic_params, shift_params
 
 @cocotb.test()
 async def top_alu_test(dut):
@@ -24,29 +25,58 @@ async def test_alu_op(dut):
   dut.scan_in.value = 0
   await reset(dut)
 
-  test_pattern=ScanPattern()
-  test_pattern.set_ctrl_state(15) # default case
-  test_pattern.set_alu_op(ALU_OP.ADD.value)
-  test_pattern.set_reg_sel_1(0)
-  test_pattern.set_reg_sel_2(1)
-  test_pattern.set_reg_sel_in(3)
-  test_pattern.set_reg_op(REGISTERS_OP.REG_WRITE.value)
-  test_pattern.set_op_done_out(1)
-  test_pattern.set_mux_sel(MUX_SEL.MUX_ALU.value)
-  test_pattern.set_register(0, 23)
-  test_pattern.set_register(1, 27)
+  params = arithmetic_params + logic_params + shift_params
+  for param in params:
+      test_pattern=ScanPattern()
+      test_pattern.set_ctrl_state(15) # default case
+      test_pattern.set_alu_op(param.op.value)
+      test_pattern.set_reg_sel_1(0)
+      test_pattern.set_reg_sel_2(1)
+      test_pattern.set_reg_sel_in(3)
+      test_pattern.set_reg_op(REGISTERS_OP.REG_WRITE.value)
+      test_pattern.set_op_done_out(1)
+      test_pattern.set_mux_sel(MUX_SEL.MUX_ALU.value)
+      test_pattern.set_register(0, param.reg1)
+      test_pattern.set_register(1, param.reg2)
 
+
+      await shift_pattern_in(dut, test_pattern)
+      
+      await RisingEdge(dut.clock)
+      await RisingEdge(dut.clock)
+
+      assert dut.reg_d.value == param.expected_result, f"Failed: {param.test_name}"
+
+      output_pattern = await shift_pattern_out(dut)
+
+
+
+@cocotb.test()
+async def test_register_write(dut):   
+  clock = Clock(dut.clock, 2, units="ns")
+  cocotb.start_soon(clock.start())
+  dut.scan_in.value = 0
+  await RisingEdge(dut.clock)
+  await reset(dut)
+  
+  test_pattern = ScanPattern()
+  test_pattern.set_register(0, 5)
+  test_pattern.set_alu_op(0) # THR
+  test_pattern.set_reg_sel_1(0)
+  test_pattern.set_reg_sel_in(1) # REG_B
+  test_pattern.set_reg_op(1) # WRITE
 
   await shift_pattern_in(dut, test_pattern)
-  
-  await RisingEdge(dut.clock)
-  await RisingEdge(dut.clock)
 
-  assert dut.reg_d.value == 50, f"ALU_OP ADD failed: Expected 50, got {dut.reg_d.value}"
+  await RisingEdge(dut.clock)
 
   output_pattern = await shift_pattern_out(dut)
 
-  assert output_pattern._regs[3] == 50, f"ALU_OP ADD failed: Expected 50, got {output_pattern._regs[2]}"
+  assert output_pattern._regs[1] == 5
+
+
+
+
 
 @cocotb.test()
 async def test_mapped_register_bank(dut):
